f5c4ba845cf924ebc78bfa8c459d5f64086bb881
Leytek <leytek@yandex.ru>
2022-06-05 18:44:28 +0300

    feat: implement delete

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,3 +1,14 @@
+import fs from 'fs/promises';
+import path from 'path';
+import url from 'url'
+
+const _dirname = path.dirname(url.fileURLToPath(import.meta.url));
+
 export const remove = async () => {
-    // Write your code here 
+  const filePath = path.join(_dirname, 'files', 'fileToRemove.txt');
+  fs.rm(filePath).catch(e => {
+    throw new Error('FS operation failed');
+  });
 };
+
+remove();
