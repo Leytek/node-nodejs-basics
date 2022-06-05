b0f49ef6eed4291ce217a7ec9d6f882550923bcd
Leytek <leytek@yandex.ru>
2022-06-05 23:16:08 +0300

    feat: implement zip

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,3 +1,21 @@
+import fs from 'fs/promises';
+import path from 'path';
+import url from 'url';
+import zlib from 'zlib';
+import events from 'events';
+
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+
 export const decompress = async () => {
-    // Write your code here 
+  const fileFromPath = path.join(__dirname, 'files', 'archive.gz');
+  const fileToPath = path.join(__dirname, 'files', 'fileToCompress.txt');
+  const fileFrom = await fs.open(fileFromPath);
+  const fileTo = await fs.open(fileToPath, 'w');
+  const fileFromStream = fileFrom.createReadStream();
+  const fileToStream = fileTo.createWriteStream();
+  const gz = zlib.createUnzip();
+
+  fileFromStream.pipe(gz).pipe(fileToStream);
 };
+
+decompress();
