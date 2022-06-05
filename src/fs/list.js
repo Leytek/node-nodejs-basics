46f9cf0c390a411824c96c0759873706bcb309d8
Leytek <leytek@yandex.ru>
2022-06-05 19:04:00 +0300

    feat: implement list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,3 +1,16 @@
+import fs from 'fs/promises';
+import path from 'path';
+import url from 'url'
+
+const _dirname = path.dirname(url.fileURLToPath(import.meta.url));
+
 export const list = async () => {
-    // Write your code here 
+  const filesDir = path.join(_dirname, 'files');
+  const files = await fs.readdir(filesDir).catch(e => {
+    throw new Error('FS operation failed');
+  });
+
+  console.log(files);
 };
+
+list();
