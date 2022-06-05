138f26e4f31e2d0f1341710e1e85b8beaca07699
Leytek <leytek@yandex.ru>
2022-06-05 19:39:48 +0300

    feat: implement read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,3 +1,17 @@
+import fs from 'fs/promises';
+import path from 'path';
+import url from 'url'
+
+const _dirname = path.dirname(url.fileURLToPath(import.meta.url));
+
 export const read = async () => {
-    // Write your code here 
+  const filePath = path.join(_dirname, 'files', 'fileToRead.txt');
+  const file = await fs.open(filePath).catch(e => {
+    throw new Error('FS operation failed');
+  });
+
+  const fileStream = file.createReadStream();
+  fileStream.pipe(process.stdout);
 };
+
+read();
