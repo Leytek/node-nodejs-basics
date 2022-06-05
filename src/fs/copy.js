599da385702044da66421d7ff63953f050edbb68
Leytek <leytek@yandex.ru>
2022-06-05 17:30:31 +0300

    feat: implement copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,3 +1,21 @@
+import fs from 'fs/promises';
+import path from 'path';
+import url from 'url'
+
+const _dirname = path.dirname(url.fileURLToPath(import.meta.url));
+
 export const copy = async () => {
-    // Write your code here 
+  const dir = await fs.readdir(_dirname);
+  const dirFromName = 'files';
+  const dirToName = 'files_copy';
+
+  if(dir.includes(dirToName) || !dir.includes(dirFromName)) {
+    throw new Error('FS operation failed');
+  }
+
+  const dirFromPath = path.join(_dirname, dirFromName);
+  const dirToPath = path.join(_dirname, dirToName);
+  fs.cp(dirFromPath, dirToPath, {recursive: true});
 };
+
+copy();
