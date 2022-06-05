058792e20adf41d44a08c21eef1209f967bb1219
Leytek <leytek@yandex.ru>
2022-06-05 20:03:05 +0300

    feat: implement env

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,3 +1,9 @@
 export const parseEnv = () => {
-    // Write your code here 
+  for(let key in process.env) {
+    if(/RSS_.*/.test(key)) {
+      console.log(`${key}=${process.env[key]}`);
+    }
+  }
 };
+
+parseEnv();
