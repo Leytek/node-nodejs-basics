6f8709725a40d596b0a5a2544f279b5e1ace217f
Leytek <leytek@yandex.ru>
2022-06-05 23:59:07 +0300

    fix: fix cli

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,8 +1,10 @@
 export const parseArgs = () => {
+  let str = '';
   process.argv.forEach((arg, i) => {
-    if(/--.*/.test(arg))
-      console.log(arg.slice(2), 'is', process.argv[i + 1]);
+    if(/^--[^-]+/.test(arg))
+      str += `${arg.slice(2)} is ${process.argv[i + 1]}, `;
   });
+  console.log(str.slice(0, -2));
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,8 +1,10 @@
 export const parseEnv = () => {
+  let str = '';
   for(let key in process.env) {
-    if(/RSS_.*/.test(key))
-      console.log(`${key}=${process.env[key]}`);
+    if(/^RSS_.+/.test(key))
+      str += `${key}=${process.env[key]}; `;
   }
+  console.log(str.slice(0, -2));
 };
 
 parseEnv();
