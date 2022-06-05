71878335d6197abdc4bcd58bfaf7e4f5ab6b53de
Leytek <leytek@yandex.ru>
2022-06-05 18:09:10 +0300

    feat: implement rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,3 +1,22 @@
+import fs from 'fs/promises';
+import path from 'path';
+import url from 'url'
+
+const _dirname = path.dirname(url.fileURLToPath(import.meta.url));
+
 export const rename = async () => {
-    // Write your code here 
+  const filesDir = path.join(_dirname, 'files');
+  const files = await fs.readdir(filesDir);
+  const fileOldName = 'wrongFilename.txt';
+  const fileNewName = 'properFilename.md';
+
+  if(files.includes(fileNewName) || !files.includes(fileOldName)) {
+    throw new Error('FS operation failed');
+  }
+
+  const fileOldPath = path.join(filesDir, fileOldName);
+  const fileNewPath = path.join(filesDir, fileNewName);
+  fs.rename(fileOldPath, fileNewPath);
 };
+
+rename();
