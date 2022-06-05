95b8797a8be380c34fec26866a68d12105daf5c1
Leytek <leytek@yandex.ru>
2022-06-05 23:43:37 +0300

    feat: implement wt

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,3 +1,21 @@
+import path from 'path';
+import url from 'url';
+import os from 'os';
+import wt from 'worker_threads';
+
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+
 export const performCalculations = async () => {
-    // Write your code here
+  const baseNum = 10;
+  function runWorker(data) {
+    return new Promise((res, rej) => {
+      const worker = new wt.Worker(path.join(__dirname, 'worker.js'), {workerData: data});
+      worker.on('message', v => res({status: 'resolved', data: v}));
+      worker.on('error', _ => rej({status: 'error', data: null}));
+    });
+  }
+  const workers = Array.from({length: os.cpus().length}, (_, i) => runWorker(baseNum + i));
+  return Promise.all(workers);
 };
+
+console.log(await performCalculations());
