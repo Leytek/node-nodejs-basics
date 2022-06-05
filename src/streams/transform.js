fb2d290830abee8ca36029d33a8c895f92d5241c
Leytek <leytek@yandex.ru>
2022-06-05 23:38:25 +0300

    fix: remove redundant code

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,8 +1,4 @@
-import path from 'path';
-import url from 'url';
 import stream from 'stream';
-
-const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
 
 export const transform = async () => {
   const transformStream = new stream.Transform({
