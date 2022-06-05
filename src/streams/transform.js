85e2f6aab8a3f736c55ef2ca492edc963bbc1e36
Leytek <leytek@yandex.ru>
2022-06-05 22:18:45 +0300

    feat: implement streams/transform

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,3 +1,17 @@
+import path from 'path';
+import url from 'url';
+import stream from 'stream';
+
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+
 export const transform = async () => {
-    // Write your code here 
+  const transformStream = new stream.Transform({
+    transform(chunk, encoding, callback) {
+      callback(null, chunk.toString().trim().split``.reverse().join`` + '\n');
+    }
+  });
+
+  process.stdin.pipe(transformStream).pipe(process.stdout);
 };
+
+transform();
