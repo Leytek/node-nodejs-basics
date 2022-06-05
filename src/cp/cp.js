78dce1f06673c9b7a8b17151549d127833ef7412
Leytek <leytek@yandex.ru>
2022-06-05 23:58:02 +0300

    feat: implement cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,3 +1,14 @@
+import path from 'path';
+import url from 'url';
+import cp from 'child_process';
+
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+
 export const spawnChildProcess = async (args) => {
-    // Write your code here
+  const scriptPath = path.join(__dirname, 'files', 'script.js');
+  const newArgs = [scriptPath, ...args];
+
+  cp.spawn(`node`, newArgs, {stdio: ['inherit', 'inherit', 'inherit']});
 };
+
+spawnChildProcess(['arg1', 'arg2']);
