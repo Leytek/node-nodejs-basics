63e006f47715638be611f31ff2c58dd52d9e3943
Leytek <leytek@yandex.ru>
2022-06-05 15:31:36 +0300

    feat: implement create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,20 @@
+import fs from 'fs/promises';
+import path from 'path';
+import url from 'url'
+
+const _dirname = path.dirname(url.fileURLToPath(import.meta.url));
+
 export const create = async () => {
-    // Write your code here 
+  const filesDir = path.join(_dirname, 'files');
+  const files = await fs.readdir(filesDir);
+  const fileName = 'fresh.txt';
+
+  if(files.includes(fileName)) {
+    throw new Error('FS operation failed');
+  }
+
+  const filePath = path.join(filesDir, fileName);
+  fs.writeFile(filePath, 'I am fresh and young');
 };
+
+create();
