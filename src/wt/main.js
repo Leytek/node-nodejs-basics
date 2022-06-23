bbc985e3f8ec7689387dfc868fa70992b6d2e9bd
Leytek <leytek@yandex.ru>
2022-06-23 07:44:22 +0300

    refactor: wt

--- src/wt/main.js
+++ src/wt/main.js
@@ -7,14 +7,16 @@ const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
 
 export const performCalculations = async () => {
   const baseNum = 10;
+
   function createWorker(data) {
     return new Promise(res => {
       const worker = new wt.Worker(path.join(__dirname, 'worker.js'), {workerData: data});
-      worker.on('message', v => res({status: 'resolved', data: v}));
+      worker.on('message', data => res({status: 'resolved', data: data}));
       worker.on('error', _ => res({status: 'error', data: null}));
     });
   }
-  const workers = Array.from({length: os.cpus().length}, (_, i) => createWorker(baseNum + i));
+
+  const workers = os.cpus().map((_, i) => createWorker(baseNum + i));
   return Promise.all(workers);
 };
 
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -2,12 +2,12 @@ import wt from 'worker_threads';
 
 export const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
-export const sendResult = (data) => {
-  wt.parentPort.postMessage(data);
+export const sendResult = (outputData) => {
+  wt.parentPort.postMessage(outputData);
 };
 
-const runWorker = (data) => {
-  const result = nthFibonacci(data);
+const runWorker = (inputData) => {
+  const result = nthFibonacci(inputData);
   sendResult(result);
 }
 
