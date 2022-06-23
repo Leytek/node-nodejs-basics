af62b1e7e3b64f207dc780c5557ced454d0756f3
Leytek <leytek@yandex.ru>
2022-06-23 03:46:26 +0300

    refactor: replace spawn with fork

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -6,9 +6,8 @@ const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
 
 export const spawnChildProcess = async (args) => {
   const scriptPath = path.join(__dirname, 'files', 'script.js');
-  const newArgs = [scriptPath, ...args];
 
-  cp.spawn(`node`, newArgs, {stdio: 'inherit'});
+  cp.fork(scriptPath, args);
 };
 
 spawnChildProcess(['arg1', 'arg2']);
